e9bbbd7330b365500eb41d8feb6ab5a425fd30b9
Sven <kaibutsu@posteo.org>
2023-01-17 16:07:37 +0100

    Curves work with ractive.

--- static/js/monitor.js
+++ static/js/monitor.js
@@ -1,116 +1,143 @@
 let ractive;
-let spO2VarTimer, hfVarTimer;
-
-const normalHfByAgeInDays = [
-    [31, 150],
-    [366, 120],
-    [365 * 5, 120],
-    [365 * 10, 80],
-    [Infinity, 150]
-]
-
-function calculateAgeInYears(dob) {
-    var ageDate = new Date(
-        Date.now() - dob.getTime()
-    );
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
-
-function calculateAgeInDays(dob) {
-    return (Date.now() - dob.getTime()) / (1000 * 60 * 60 * 24)
-}
-
-function getHfByDob(dob) {
-    ageInDays = calculateAgeInDays(dob);
-    for (ageHfPair in normalHfByAgeInDays) {
-        if (ageInDays < ageHfPair[0]) {
-            return ageHfPair[1];
-        }
-    }
-}
-
-const patient_data = {
-    demographics: {
-        name: "John",
-        surname: "Doe",
-        dob: new Date("1970-01-01T00:00:00"),
-        pid: "123456789",
-    },
-    vitals: {
-        hf: {
-            target: 60,
-            display: 60,
-            min: 0,
-            max: 230,
-            varFreq: 1,
-            varInterval: 2,
-            initInterval: 1
-        },
-        spO2: {
-            target: 100,
-            display: 100,
-            min: 21,
-            max: 100,
-            varFreq: 2,
-            varInterval: 1,
-            initInterval: 10
-        },
-        bpSys: {
-            target: 120,
-            display: 120,
-            min: 30,
-            max: 220,
-            varFreq: 10,
-            varInterval: null,
-            initInterval: 30
-        },
-        bpDia: {
-            target: 60,
-            display: 60,
-            min: 20,
-            max: 100,
-            varFreq: 10,
-            varInterval: null,
-            initInterval: 30
-        },
-        bf: {
-            target: 14,
-            display: 14,
-            min: 0,
-            max: 180,
-            varFreq: 5,
-            varInterval: 5,
-            initInterval: 3
-        },
-    }
-}
-
-function varyParameter(parameterStr) {
-    let newDisplay = ractive.get(parameterStr + '.target') + (2 * (Math.random() - 0.5) * ractive.get(parameterStr + '.varInterval'))
-    newDisplay = Math.round(newDisplay)
-    if (newDisplay > ractive.get(parameterStr + '.max')) {
-        newDisplay = ractive.get(parameterStr + '.max')
-    }
-    if (newDisplay < ractive.get(parameterStr + '.min')) {
-        newDisplay = ractive.get(parameterStr + '.min')
-    }
-    console.log('Changing ' + parameterStr + '.display from ' + ractive.get(parameterStr + '.display') + ' to ' + newDisplay)
-    ractive.set(parameterStr + '.display', newDisplay)
+let busTimer, signalUpdateTimer;
+let runtime = 0;
+const updateInterval = 100;
+const signalPixelsPerSecond = 200;
+const contexts = {};
+const bufferPointers = {};
+const signalsLineWidths = {
+    'ecg': 4,
+    'spO2': 4
+};
+const signalStrokeStyles = {
+    'ecg': 'lawngreen',
+    'spO2': "skyblue"
 };
 
-ractive = new Ractive({
-    target: '#ractive-target',
-    template: '#ractive-template',
-    data: { patient_data: patient_data }
-});
+class VitalDefinition {
+    constructor(target, min, max, varFreq, varAmp, initDelay) {
+        this.target = target
+        this.min = min
+        this.max = max
+        this.varFreq = varFreq
+        this.varAmp = varAmp
+        this.initDelay = initDelay
+    }
+}
 
-if (!hfVarTimer) {
-    hfVarTimer = setInterval(varyParameter, patient_data.vitals.hf.varFreq*1000, 'patient_data.vitals.hf');
-};
+class Patient {
+    firstName = "John"
+    surname = "Doe"
+    dob = new Date("1970-01-01T00:00:00")
+    pid = "123456789"
+    constructor(firstName, surname, dob, pid) {
+        this.firstName = firstName
+        this.surname = surname
+        this.dob = dob
+        this.pid = pid
+    }
 
-if (!spO2VarTimer) {
-    spO2VarTimer = setInterval(varyParameter, patient_data.vitals.spO2.varFreq*1000, 'patient_data.vitals.spO2');
-};
+    get fullName() {
+        return this.surname + ", " + this.firstName
+    }
+
+    get ymdDob() {
+        let fullMonth = (this.dob.getMonth() + 1 < 10) ? '0' + (this.dob.getMonth() + 1) : this.dob.getMonth() + 1
+        let fullDate = (this.dob.getDate() < 10) ? '0' + this.dob.getDate() : this.dob.getDate()
+        return (this.dob.getFullYear() + '-' + fullMonth + '-' + fullDate)
+    }
+
+    get ageInYears() {
+        var ageDate = new Date(
+            Date.now() - this.dob.getTime()
+        );
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    get ageInDays() {
+        return (Date.now() - this.dob.getTime()) / (1000 * 60 * 60 * 24)
+    }
+}
+
+function bufferNextCurve(parameterName, amplitude = 0.9) {
+    let signalContext = contexts[parameterName]["signal"];
+    let bufferContext = contexts[parameterName]["buffer"];
+    let bufferPointer = bufferPointers[parameterName];
+    let curveEvent = ractive.get("display.curveEvents." + parameterName)
+    let eventRate = ractive.get("display." + curveEvent)
+
+    // Adapt buffer size to one heart beat:
+    let bufferWidth = bufferContext.canvas.width = 60 / eventRate * signalPixelsPerSecond;
+    let bufferHeigth = bufferContext.canvas.height = signalContext.canvas.height - signalContext.lineWidth * 2
+
+    // Set line color and width:
+    bufferContext.strokeStyle = signalContext.strokeStyle;
+    bufferContext.lineWidth = signalContext.lineWidth;
+
+    // pleth signal height could be adjusted later:
+    let maxSigHeight = bufferHeigth * amplitude
+    let minValue = bufferHeigth - bufferContext.lineWidth
+    // calculate points to draw curve
+    if (parameterName === "spO2") {
+        points = [
+            { x: 0, y: minValue },
+            { x: 0.1 * bufferWidth, y: minValue },
+            { x: 0.2 * bufferWidth, y: minValue - maxSigHeight },
+            { x: 0.4 * bufferWidth, y: minValue - 0.4 * maxSigHeight },
+            { x: 0.7 * bufferWidth, y: minValue - 0.5 * maxSigHeight },
+            { x: 0.9 * bufferWidth, y: minValue },
+            { x: 1 * bufferWidth, y: minValue },
+        ]
+    } else {
+        points = [
+            { x: 0, y: minValue - maxSigHeight },
+            { x: 0.03 * bufferWidth, y: minValue - 0.1 * maxSigHeight },
+            { x: 0.1 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 0.2 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 0.275 * bufferWidth, y: minValue - 0.35 * maxSigHeight },
+            { x: 0.35 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 0.5 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 0.625 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 0.7 * bufferWidth, y: minValue - 0.35 * maxSigHeight },
+            { x: 0.775 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 0.9 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 0.97 * bufferWidth, y: minValue - 0.2 * maxSigHeight },
+            { x: 1 * bufferWidth, y: minValue - maxSigHeight },
+        ]
+    }
+
+    // interpolate curve
+    // from: https://stackoverflow.com/questions/7054272/how-to-draw-smooth-curve-through-n-points-using-javascript-html5-canvas
+    bufferContext.moveTo(points[0].x, points[0].y);
+    for (i = 1; i < points.length - 2; i++) {
+        var xc = (points[i].x + points[i + 1].x) / 2;
+        var yc = (points[i].y + points[i + 1].y) / 2;
+        bufferContext.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
+    }
+    bufferContext.quadraticCurveTo(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+    bufferContext.stroke();
+    bufferPointer.pos = 0;
+    bufferPointer.size = bufferWidth;
+}
+
+function animateSignal(parameterName) {
+    signalContext = contexts[parameterName]['signal']
+    bufferContext = contexts[parameterName]['buffer']
+    bufferPointer = bufferPointers[parameterName]
+    if (!(bufferPointer.pos < bufferPointer.size)) {
+        bufferNextCurve(parameterName);
+    }
+    bufferContext.save;
+    let bufferedCol = bufferContext.getImageData(bufferPointer.pos, 0, 1, signalContext.canvas.height)
+    // shift everything to the left:
+    oldImageData = signalContext.getImageData(1, 0, signalContext.canvas.width - 1, signalContext.canvas.height)
+    signalContext.putImageData(oldImageData, 0, 0);
+    // Write buffer column to right end:
+    signalContext.putImageData(bufferedCol, signalContext.canvas.width - 1, 0);
+
+    bufferPointer.pos = bufferPointer.pos + 1;
+    bufferContext.restore()
+}
 
 function initialize() {
     peer = new Peer(null, { debug: 2 });
@@ -188,4 +215,161 @@ function playSound(audio) {
     current_audio = audio;
 }
 
+function updateCanvases(parameterNames) {
+    parameterNames.forEach(parameterName => {
+        currentContext = contexts[parameterName]["signal"]
+        currentContext.canvas.width = currentContext.canvas.parentElement.clientWidth;
+        currentContext.lineWidth = signalLineWidths[parameterName];
+        currentContext.strokeStyle = signalStrokeStyles[parameterName];
+        console.log(signalLineWidths[parameterName]);
+    });
+}
 
+ractive = new Ractive({
+    target: '#ractive-target',
+    template: '#ractive-template',
+    data: {
+        patient: new Patient(
+            firstName = "John",
+            surname = "Doe",
+            dob = new Date("1970-01-01T00:00:00"),
+            pid = "123456789",
+        ),
+        vitals: {
+            hf: new VitalDefinition(
+                target = 60,
+                min = 0,
+                max = 230,
+                varFreq = 2000,
+                varAmp = 2,
+                initDelay = 1000
+            ),
+            spO2: new VitalDefinition(
+                target = 100,
+                min = 21,
+                max = 100,
+                varFreq = 2000,
+                varAmp = 1,
+                initDelay = 10000
+            ),
+            resp: new VitalDefinition(
+                target = 14,
+                min = 0,
+                max = 180,
+                varFreq = 5000,
+                varAmp = 5,
+                initDelay = 3000
+            ),
+            bpSys: new VitalDefinition(
+                target = 120,
+                min = 30,
+                max = 220,
+                varFreq = 10000,
+                varAmp = null,
+                initDelay = 30000
+            ),
+            bpDia: new VitalDefinition(
+                target = 60,
+                min = 20,
+                max = 100,
+                varFreq = 10000,
+                varAmp = null,
+                initDelay = 30000
+            ),
+            temp: new VitalDefinition(
+                target = 37.5,
+                min = 36.0,
+                max = 41.5,
+                varFreq = 5000,
+                varAmp = 0.5,
+                initDelay = 30000
+            ),
+        },
+        display: {
+            curveEvents: {
+                ecg: 'hf',
+                spO2: 'hf',
+                resp: 'resp'
+            },
+            triggers: {
+                ecg: true,
+                spO2: true,
+                resp: false,
+                bp: false,
+                temp: false,
+            }
+        },
+        connected: false,
+    }
+});
+
+function initCanvasContexts(parameterName) {
+    contexts[parameterName] = {}
+    contexts[parameterName]['signal'] = ractive.find("#" + parameterName + "SigCanvas").getContext("2d", { willReadFrequently: true });
+    contexts[parameterName]['buffer'] = ractive.find("#" + parameterName + "BufferCanvas").getContext("2d", { willReadFrequently: true });
+}
+
+function varyParameter(parameterName) {
+    let fullParameterStr = 'vitals.' + parameterName
+    let parameterMax = ractive.get(fullParameterStr + '.max')
+    let parameterMin = ractive.get(fullParameterStr + '.min')
+    let newDisplay = Math.round(
+        ractive.get(fullParameterStr + '.target')
+        + (2 * (Math.random() - 0.5) * ractive.get(fullParameterStr + '.varAmp'))
+    )
+    newDisplay = Math.max(
+        Math.min(
+            newDisplay, parameterMax
+        ), parameterMin
+    )
+
+    ractive.set('display.' + parameterName, newDisplay)
+};
+
+function fitToContainer(canvas) {
+    canvas.height = canvas.parentElement.clientHeight;
+    canvas.width = canvas.parentElement.clientWidth;
+}
+
+function updateMonitor(parameterNames = []) {
+    runtime += updateInterval;
+    ractive.set('display.dateTime', new Date().toLocaleString());
+    parameterNames.forEach(parameter => {
+        if (
+            runtime % ractive.get("vitals." + parameter + ".varFreq") < updateInterval
+        ) {
+            varyParameter(parameter)
+        }
+    });
+}
+
+function drawSignals() {
+    let triggers = ractive.get('display.triggers');
+    Object.entries(triggers).forEach(([parameterName, triggerState]) => {
+        if (triggerState) {
+            animateSignal(parameterName)
+        }
+    })
+}
+
+if (!busTimer) {
+    busTimer = setInterval(updateMonitor, updateInterval, ['hf', 'spO2'])
+}
+
+if (!signalUpdateTimer) {
+    signalUpdateTimer = setInterval(drawSignals, 1000 / signalPixelsPerSecond);
+}
+
+initCanvasContexts("ecg");
+initCanvasContexts("spO2");
+bufferPointers["ecg"] = {
+    pos: 0,
+    size: 0,
+};
+bufferPointers["spO2"] = {
+    pos: 0,
+    size: 0,
+};
+window.addEventListener("resize", () => { updateCanvases(['ecg', 'spO2']) });
+updateMonitor()
+updateCanvases(['ecg', 'spO2']);
